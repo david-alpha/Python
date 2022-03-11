e18ce6315ed109a9c5fe3340f7c510f536ddc5e5
pintrou <you@example.com>
2022-03-11 10:32:38 +0000

    exercice 1

--- TP1Ex1Q2.py
+++ TP1Ex1Q2.py
@@ -5,10 +5,10 @@ Created on Fri Mar 11 11:28:33 2022
 """
 
 VENTES={"Martin":38, "Dupont":52, "Durand":26, "Perrin":64,"Blanc":42}
-somme = 0
+max = 0
 
 for cle, valeur in VENTES.items():
-    somme += valeur
-print(somme)
-
-
+    if valeur > max :
+            max=valeur
+            nom = cle
+print('le meilleur vendeur est ', nom, 'avec le total', max)
